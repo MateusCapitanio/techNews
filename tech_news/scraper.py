1e18512001be07f8d27b4e4efa619c54e93bdcad
Mateus Capitanio <mateusjmj92@gmail.com>
2022-10-06 18:28:17 -0300

    Requisito 4 [Finalizado]

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -40,48 +40,36 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     selector = Selector(text=html_content)
-    url = selector.css(
-        'head link[rel=canonical]::attr(href)'
-    ).get()
+    url = selector.css('head link[rel=canonical]::attr(href)').get()
     title = selector.css(
         '.entry-header-inner .entry-title::text'
-    ).get()
-    timestamp = selector.css(
-        '.site-primary .site-content .entry-header .post-meta .meta-date::text'
-    ).get()
-    author = selector.css(
-        '.entry-header-inner .post-meta .meta-author .author a::text'
-    ).get()
+    ).get().rstrip()
+    timestamp = selector.css('.meta-date::text').get()
+    author = selector.css('li.meta-author .author a::text').get()
     comments_count = selector.css(
         'div.post-comments h5.title-block::text'
     ).get()
     if comments_count is None:
         comments_count = 0
-    else:
-        comments_count = comments_count[4]
-    text = selector.css(
-        'div.entry-content p:first-child *::text'
-    ).getall()
-    fullText = "".join(text)
+    text = selector.xpath("string(//p)").get().rstrip()
     tags = selector.css(
         'div.entry-content-wrap .post-tags ul li a::text'
     ).getall()
-    category = selector.css(
-        'div.meta-category .category-style .label::text'
-    ).get()
+    if tags is None:
+        tags = []
+    category = selector.css('div.meta-category a span.label::text').get()
 
-    result = {
+    data = ({
         "url": url,
         "title": title,
         "timestamp": timestamp,
         "writer": author,
         "comments_count": comments_count,
-        "summary": fullText,
+        "summary": text,
         "tags": tags,
         "category": category,
-    }
-    print(result)
-    return result
+    })
+    return data
 
 
 # Requisito 5
